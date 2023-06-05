5edd87d08783f1cfb2b406ebac0d75b364aa5a77
postigoj <josepostigo99@gmail.com>
2023-06-05 17:31:28 -0300

    add database conection

---
+++ Api.Test/Models/DataBaseContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Test.Models;
+
+public partial class DataBaseContext : DbContext
+{
+    public DataBaseContext()
+    {
+    }
+
+    public DataBaseContext(DbContextOptions<DataBaseContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    public virtual DbSet<UsuarioAcceso> UsuarioAccesos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.ToTable("cliente");
+
+            entity.HasIndex(e => e.Dni, "IX_cliente_dni_unique").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("apellido");
+            entity.Property(e => e.Cuit).HasColumnName("cuit");
+            entity.Property(e => e.Direccion)
+                .HasMaxLength(250)
+                .IsUnicode(false)
+                .HasColumnName("direccion");
+            entity.Property(e => e.Dni).HasColumnName("dni");
+            entity.Property(e => e.Email)
+                .HasMaxLength(200)
+                .IsUnicode(false)
+                .HasColumnName("email");
+            entity.Property(e => e.FechaNacimiento)
+                .HasColumnType("date")
+                .HasColumnName("fecha_nacimiento");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Telefono).HasColumnName("telefono");
+        });
+
+        modelBuilder.Entity<UsuarioAcceso>(entity =>
+        {
+            entity.ToTable("usuario_acceso");
+
+            entity.HasIndex(e => e.Usuario, "IX_usuario_acceso_usuario_unique").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Password)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasColumnName("password");
+            entity.Property(e => e.Usuario)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasColumnName("usuario");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- Api.Test/Program.cs
+++ Api.Test/Program.cs
@@ -1,7 +1,19 @@
+using Api.Test.Interface;
+using Api.Test.Models;
+using Api.Test.Repository;
+using Api.Test.Service;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//Database connection
+builder.Services.AddDbContext<DataBaseContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
+
+builder.Services.AddTransient<IClientes, ClienteRepository>();
+builder.Services.AddScoped<ClienteService>();
+builder.Services.AddScoped<ClienteRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
