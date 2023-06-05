4f34ed1a8da4c3c67b6c8b63217d7aaeaaa7c2ff
postigoj <josepostigo99@gmail.com>
2023-06-05 20:02:56 -0300

    fix isValidCuit

--- Api.Test/Models/Cliente.cs
+++ Api.Test/Models/Cliente.cs
@@ -46,23 +46,23 @@ public partial class Cliente
 
     public bool IsValidCuit()
     {
-        string cuitWithoutDigit = Cuit?.ToString(); // Convert nullable long to string
-        if (string.IsNullOrEmpty(cuitWithoutDigit) || cuitWithoutDigit.Length != 11)
+        string newCuit = Cuit?.ToString(); // Convertir numero a string
+        if (string.IsNullOrEmpty(newCuit) || newCuit.Length != 11)
         {
             return false; 
         }
 
-        cuitWithoutDigit = cuitWithoutDigit.Substring(0, cuitWithoutDigit.Length - 1); // Remove the check digit
-        int[] multiplier = { 5, 4, 3, 2, 7, 6, 5, 4, 3, 2 };
+        newCuit = newCuit.Substring(0, newCuit.Length - 1); // separar digito verificador
+        int[] multiplicador = { 5, 4, 3, 2, 7, 6, 5, 4, 3, 2 };
 
         int sum = 0;
-        for (int i = 0; i < cuitWithoutDigit.Length; i++)
+        for (int i = 0; i < newCuit.Length; i++)
         {
-            sum += int.Parse(cuitWithoutDigit[i].ToString()) * multiplier[i];
+            sum += int.Parse(newCuit[i].ToString()) * multiplicador[i];
         }
 
-        int remainder = sum % 11;
-        int checkDigit = (remainder == 0) ? 0 : (11 - remainder) % 11;
+        int resto = sum % 11;
+        int checkDigit = (resto == 0) ? 0 : (11 - resto) % 11;
 
         int lastDigit = int.Parse(Cuit.Value.ToString()[Cuit.Value.ToString().Length - 1].ToString());
 
