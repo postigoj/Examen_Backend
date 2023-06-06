983ae186fd85a37e4b71e739587b71b84a9a869c
postigoj <josepostigo99@gmail.com>
2023-06-06 14:24:23 -0300

    add userRepository getuser method

---
+++ Api.Test/Repository/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using Api.Test.Interface;
+using Api.Test.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Test.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        readonly DataBaseContext _dbContext = new();
+
+        public UserRepository(DataBaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddUserAsync(UsuarioAcceso user)
+        {
+            {
+                try
+                {
+                    _dbContext.UsuarioAccesos.Add(user);
+                    await _dbContext.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+                    throw new ApplicationException("Fallo al regristar usuario", ex);
+                }
+            }
+        }
+
+        public async Task<UsuarioAcceso> GetUser(string usuario, string password)
+        {
+            return await _dbContext.UsuarioAccesos.FirstOrDefaultAsync(u => u.Usuario == usuario && u.Password == password );
+        }
+    }
+}
