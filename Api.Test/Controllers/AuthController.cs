daa43a12699d652fbef06c983f7b8f8bc7a71d41
postigoj <josepostigo99@gmail.com>
2023-06-06 14:22:16 -0300

    add Authcontroller login user

---
+++ Api.Test/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using Api.Test.Models;
+using Api.Test.Service;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace Api.Test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    
+        private readonly AuthService _authService;
+
+
+        public AuthController( AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(UsuarioAcceso _userData)  {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+
+  
+              var token = await _authService.AuthenticateUser(_userData.Usuario, _userData.Password);
+
+                if (token != null)
+                {
+                    return Ok(token);
+                }
+                else
+                {
+                    return BadRequest("Credenciales invalidas");
+                }
+            
+            //else
+            //{
+            //    return BadRequest();
+            //}
+        }
+    }
+}
