2a62275b7a2a0286430269f7eb65f3828ba6b1c7
postigoj <josepostigo99@gmail.com>
2023-06-06 14:19:17 -0300

    add models usuario acceso

---
+++ Api.Test/Models/UsuarioAcceso.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Test.Models;
+
+public partial class UsuarioAcceso
+{
+    public short Id { get; set; }
+
+    [Required]
+    [MaxLength(10, ErrorMessage = "La contraseña debe tener una longitud mínima de 8 caracteres.")] 
+    public string Usuario { get; set; } = null!;
+
+    [Required]
+    [MaxLength(10, ErrorMessage = "La contraseña debe tener una longitud mínima de 8 caracteres.")]
+    public string Password { get; set; } = null!;
+}
+
+
