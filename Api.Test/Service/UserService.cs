3929ef5560cde77ad37cad3ca453c406b9d5ad0d
postigoj <josepostigo99@gmail.com>
2023-06-06 15:41:53 -0300

    add user service register service

---
+++ Api.Test/Service/UserService.cs
@@ -0,0 +1,30 @@
+﻿using Api.Test.Models;
+using Api.Test.Repository;
+
+namespace Api.Test.Service
+{
+    public class UserService
+    {
+        private readonly UserRepository _UserRepository;
+
+        public UserService(UserRepository userRepository)
+        {
+            _UserRepository = userRepository;
+        }
+
+        public async Task<bool> CreateUserAsync(UsuarioAcceso user)
+        {
+            var checkUser = await _UserRepository.CheckUser(user.Usuario);
+
+            if (checkUser)
+            {
+                return false;
+            }
+
+            await _UserRepository.AddUserAsync(user);
+                return true;
+            
+        }
+
+    }
+}
