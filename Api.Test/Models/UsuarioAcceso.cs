b583ef90fee4cd1dbb43a7ea5a67bf0de500d2b1
postigoj <josepostigo99@gmail.com>
2023-06-06 14:21:21 -0300

    add validattion usuario acceso

--- Api.Test/Models/UsuarioAcceso.cs
+++ Api.Test/Models/UsuarioAcceso.cs
@@ -7,11 +7,11 @@ public partial class UsuarioAcceso
     public short Id { get; set; }
 
     [Required]
-    [MaxLength(10, ErrorMessage = "La contraseña debe tener una longitud mínima de 8 caracteres.")] 
+    [MaxLength(10, ErrorMessage = "La contraseña debe tener una longitud maxima de 10 caracteres.")] 
     public string Usuario { get; set; } = null!;
 
     [Required]
-    [MaxLength(10, ErrorMessage = "La contraseña debe tener una longitud mínima de 8 caracteres.")]
+    [MaxLength(10, ErrorMessage = "La contraseña debe tener una longitud maxima de 10 caracteres.")]
     public string Password { get; set; } = null!;
 }
 
